a148c38e07706c9ec95e9f6232b7951fd1baf3ec
shkgitun <162606606+shkgitun@users.noreply.github.com>
2024-03-19 17:12:02 -0400

    Create main.js

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,26 @@
+// document.write method
+document.write("Hello, World!");
+
+//window.alert method
+window.alert("How are you?");
+
+// This is a string
+var A ="I am happy"; 
+
+document.write(A);
+
+document.write("He asked, \"What does love mean to you?\" ");
+
+ // This is an example of multiple variables
+var B = "Bold", C="Concantenate", F="Function";
+document.write(C);
+
+// This is an expression
+document.write(32 + 34);
+
+//creating two variables
+var Sent1 = "This is the beginning of the string";
+var Sent2 = " and this is the end of the string";
+
+//displaying the variables using the document.write method
+document.write(Sent1 + Sent2);
